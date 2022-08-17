6a6857176ddd6c6b244caab428a7166c3e7ad458
GLADYS <mungaiglado106@gmail.com>
2022-08-17 22:55:03 +0300

    commit tree f13ccf618dea48c03c30d295e8f6d66e4106d1a1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+def  name=(dog_name)
+    @this_dogs_name = dog_name       #sets the dog name
+    
 end
+def name
+   @this_dogs_name                  #gets the dog name
+    end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
